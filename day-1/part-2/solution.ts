414a7ffdbce54b892d2b1f7b2746d21c4e34b526
Marcelo Santos <marcelsud@gmail.com>
2023-12-02 17:27:18 -0300

    chore: removed unused code

--- day-1/part-2/solution.ts
+++ day-1/part-2/solution.ts
@@ -1,31 +1,3 @@
-const lettersMap = {
-  one: "1",
-  two: "2",
-  three: "3",
-  four: "4",
-  five: "5",
-  six: "6",
-  seven: "7",
-  eight: "8",
-  nine: "9",
-};
-
-export function translate(phrase: string): string {
-  let newPhrase = "";
-  for (let i = 0; i < phrase.length; i++) {
-    let matched = false;
-    for (let [key, value] of Object.entries(lettersMap)) {
-      if (phrase.slice(i, i + key.length) === key) {
-        newPhrase += value;
-        i += key.length - 1;
-        matched = true;
-      }
-    }
-    if (!matched) newPhrase += phrase[i];
-  }
-  return newPhrase;
-}
-
 export function convert(line: string): number {
   const digits = [
     "one",
@@ -49,11 +21,8 @@ export function convert(line: string): number {
 }
 
 export function solution(input: string): number {
-  return (
-    input
-      .split("\n")
-      // .map((line: string) => translate(line))
-      .map((phrase: string) => convert(phrase))
-      .reduce((a, b) => a + b)
-  );
+  return input
+    .split("\n")
+    .map((phrase: string) => convert(phrase))
+    .reduce((a, b) => a + b);
 }
